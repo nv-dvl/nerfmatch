d8df072211b9838860e2fef4726edbfe449c081c
Qunjie Zhou <qunjiez@nvidia.com>
2024-10-09 10:24:47 +0000

    Migrate data and models for public download (#1)

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,20 +1,20 @@
 # Download image retrieval pairs
-# https://drive.google.com/file/d/1ZhBjKfP6jVOUvEffX5SUosUdBYpydaBS/view?usp=drive_link
-gdown 1ZhBjKfP6jVOUvEffX5SUosUdBYpydaBS
+# https://drive.google.com/file/d/1-gOqAA0-sdiUZmQLRYlGprQdk7ttldfE/view?usp=drive_link
+gdown 1-gOqAA0-sdiUZmQLRYlGprQdk7ttldfE
 tar -xvzf pairs.tar.gz
 rm pairs.tar.gz
 
 
 # Download data annotations
-# https://drive.google.com/file/d/1iMuZU5GW0i4ySOuVDKWYdsehRNdTd-tE/view?usp=drive_link
-gdown 1iMuZU5GW0i4ySOuVDKWYdsehRNdTd-tE
+# https://drive.google.com/file/d/1iHwsGwKXZWHxF_o9OMZUnGYKCopO-cKS/view?usp=drive_link
+gdown 1iHwsGwKXZWHxF_o9OMZUnGYKCopO-cKS
 tar -xvzf  annotations.tar.gz
 rm  annotations.tar.gz
 
 
 # Download SAM masks for training NeRF on Cambridge Landmarks
-# https://drive.google.com/file/d/1XXCJSRXkaw1m9jYQw9Y45BCjp7Ei2a99/view?usp=drive_link
-gdown 1XXCJSRXkaw1m9jYQw9Y45BCjp7Ei2a99
+# https://drive.google.com/file/d/1lGgLcA6kZPJcOOrtMFhUml2KpOYio2MO/view?usp=drive_link
+gdown 1lGgLcA6kZPJcOOrtMFhUml2KpOYio2MO
 tar -xvzf  mask_preprocessed.tar.gz
 rm  mask_preprocessed.tar.gz
 
--- pretrained/download_pretrained.sh
+++ pretrained/download_pretrained.sh
@@ -1,12 +1,12 @@
 # Download pretrained NeRF models
-# https://drive.google.com/file/d/1-sSdIFNAH5AuR9irRWf1W1lBZ4qh9HP_/view?usp=drive_link
-gdown 1-sSdIFNAH5AuR9irRWf1W1lBZ4qh9HP_
+# https://drive.google.com/file/d/1Y2gJeDVeOLk9yNbPKFnnjPzu7poXgD03/view?usp=drive_link
+gdown 1Y2gJeDVeOLk9yNbPKFnnjPzu7poXgD03
 tar -xvzf nerf.tar.gz
 rm nerf.tar.gz
 
 
 # Download pretrained NeRFMatch models
-# https://drive.google.com/file/d/1uyrMoyEPpFj0tNn2dZg1MOTr8lnpPBR6/view?usp=drive_link
-gdown 1uyrMoyEPpFj0tNn2dZg1MOTr8lnpPBR6 
+# https://drive.google.com/file/d/1PJTGz-TTDwhGpyQw9ZVTQBlzg3ru1zJt/view?usp=drive_link
+gdown 1PJTGz-TTDwhGpyQw9ZVTQBlzg3ru1zJt
 tar -xvzf nerfmatch.tar.gz
 rm  nerfmatch.tar.gz
